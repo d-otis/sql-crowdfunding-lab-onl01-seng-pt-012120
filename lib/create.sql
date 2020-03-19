0cf6e8651a59fc6263ce5ff8d3df8e9f2c3b17a3
Dan Foley <dan@dan-foley.com>
2020-03-19 17:25:28 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,3 +6,13 @@ CREATE TABLE projects (
   start_date TEXT,
   end_date TEXT
 );
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  
+);
