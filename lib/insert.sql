d871eab3cbf4f4aa3a9802b727f49acbdff32df3
Dan Foley <dan@dan-foley.com>
2020-03-19 17:35:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-("Super Cool Music Video", "video", 3000, "20200126", "20200226")
-()
-()
-()
-()
-()
-()
-()
-()
-();
+("Super Cool Music Video", "video", 3000, "20200126", "20200226"),
+("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Children's Book", "book", 4000, "20200501", "20200630"),
+("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Fashion Show", "live event", 5000, "20200301", "20200330");
