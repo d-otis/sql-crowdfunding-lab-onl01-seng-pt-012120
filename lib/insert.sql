abde8e36060a498760cbcc6f6aaffc96e9734011
Dan Foley <dan@dan-foley.com>
2020-03-19 17:38:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,8 +4,8 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 ("Children's Book", "book", 4000, "20200501", "20200630"),
 ("Dan's Short Film", "film", 5000, "20200401", "20200320"),
 ("Kid's Puzzle Game", "toy", 5000, "20200201", "20200228"),
-("Dylan's Album", "music album", 10000, "20200301", "20200330"),
-("Fashion Show", "live event", 5000, "20200301", "20200330"),
+("Dylan's Album", "music album", 10000, "20200101", "20200630"),
+("Voice to MIDI Software", "software", 25000, "20200101", "20200330"),
 ("Fashion Show", "live event", 5000, "20200301", "20200330"),
 ("Fashion Show", "live event", 5000, "20200301", "20200330"),
 ("Fashion Show", "live event", 5000, "20200301", "20200330");
